322dfaccc3b48ed98d99407a48095a6acb06d245
RBurkhardt87 <r.burkhardt.ibclc@gmail.com>
2024-06-10 19:32:20 -0500

    COMPLETED CHAPTER 4 EXERCISE

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,15 +1,3 @@
-// Declare and assign the variables below
-
-// Use console.log to print the 'typeof' each variable. Print one item per line.
-
-// Calculate a space mission below
-
-// Print the results of the space mission calculations below
-
-// Calculate a trip to the moon below
-
-// Print the results of the trip to the moon below
-
 //Setting the variables and values from the chart
 let spaceShuttle = 'Determination';
 let shuttleSpeedMph = 17500;
