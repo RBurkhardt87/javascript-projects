8b4d3a697cd8f3f0a50339afab4ccf06f87d2fc3
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-07-02 00:03:33 -0500

    try it done

--- more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
+++ more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
@@ -2,6 +2,25 @@ let names = ["Chris", "Jim", "Sally", "Blake", "Paul", "John", "Courtney", "Carl
 
 // TODO: Write a mapping function
 // and pass it to .map()
-let firstInitials = names.map();
 
+let getInitial = function(n) {
+for (let i = 0; i < names.length; i++){
+  return n[i];
+}
+};
+let firstInitials = names.map(getInitial);
+
+console.log(names);
 console.log(firstInitials);
+
+
+
+// let firstInitials = names.map(function (element){
+//     // console.log(element[0]);
+    
+//     for (let i = 0; i < names.length; i++){
+//     return element[i];
+//     }
+// });
+
+// console.log(firstInitials);
