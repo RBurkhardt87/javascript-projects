38526d9fef152b7d63f67f59e7bfebe642b9f53b
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-07-10 02:14:08 -0500

    part 1 completed

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,53 @@
+
+
 // Code your selectRandomEntry function here:
 
+function selectRandomEntry(array){
+let index = Math.floor(Math.random()*array.length);
+  return array[index];
+}
+
+
+let idNumbers = [291, 414, 503, 599, 796, 890];
+let newArray = []; 
+
+  while(newArray.length < 3) {
+  let number = selectRandomEntry(idNumbers);
+    
+  if (!newArray.includes(number)) {
+    newArray.push(number); 
+  }
+
+  }
+
+console.log(newArray);
 
 // Code your buildCrewArray function here:
 
 
-let idNumbers = [291, 414, 503, 599, 796, 890];
+function buildCrew(ids, candidates) {
+  let crew = [];
+
+  // ----------This is done with single loop----------------//
+  // for(let i = 0; i < candidates.length; i++) {
+  //     if (ids.includes(candidates[i].astronautID)) {                                              
+  //    crew.push(candidates[i]);
+  //    }
+  //  }
+  
+
+
+  //-----------Now let's try a nested loop-----------------//
+    for (let i = 0; i < ids.length; i++) {                    //outside loop is going over the ids. For each element a inner loop will run
+      for (let j = 0; j < candidates.length; j++) {           // This is what we did during studio... it didn't work for me because i didn't use j++ 
+        if (candidates[j].astronautID === ids[i]) {           // I automatically did i++. Once changed, the program ran.
+          crew.push(candidates[j]);
+        }
+      }
+    }
+   return crew;
+}
+
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -52,4 +95,7 @@ let candidateF = {
 
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
-// Code your template literal and console.log statements:
+// Code your template literal and console.log statements
+let finalCrew = buildCrew(newArray, animals);
+console.log(`${finalCrew[0].name}, ${finalCrew[1].name}, and ${finalCrew[2].name} are going to space!`);
+
