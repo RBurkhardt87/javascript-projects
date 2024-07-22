8c0355b4525859d768b80fff17be4cdfee808e4f
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-07-22 14:46:19 -0500

    Should be completed

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,7 +4,17 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+};
+
+try { 
+ if (gradeLabs()) {
+  } catch (err) {
+     console.log("We caught an error! 'runLab' is NOT defined")
+  };
 }
+
+
+
 
 let studentLabs = [
   {
@@ -22,3 +32,32 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
+
+
+
+//-------------------------------------------------------
+
