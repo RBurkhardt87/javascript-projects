f243390069a952c44d304e6d9586f0c4753bf1de
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-08-08 14:00:06 -0500

    Exercist complete besides bonus mission

--- user-input-with-forms/chapter-examples/form-validation/script.js
+++ user-input-with-forms/chapter-examples/form-validation/script.js
@@ -1,1 +1,13 @@
 //Add Your Code Below
+<script>
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+        let usernameInput = document.querySelector("input[name=username]");
+        let teamName = document.querySelector("input[name=team]");
+        if (usernameInput.value === "" || teamName.value === "") {
+            alert("All fields are required!");
+        }
+        });
+    });
+</script>
--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,9 @@
 //Code Your Solution Below
+
+
+form.addEventListener("submit", function(event){
+    if (testName.value ==="" ){
+        window.alert("Must have a valid name eneterd.");
+        event.preventDefault();
+    }
+});
